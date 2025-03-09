4799d615702e0e4e4feacb225931deb53d269939
abePclWaseda <abe@pcl.cs.waseda.ac.jp>
2025-03-09 15:52:08 +0900

    窓関数をかけてフーリエ変換する 処理追加

--- voice_actor_recognition.py
+++ voice_actor_recognition.py
@@ -8,6 +8,7 @@ import soundfile as sf
 
 base_path = '/mnt/kiso-qnap/abe/m1/voice_actor_recognition/data/'
 
+# 1フレームを切り出す
 data, fs = sf.read(base_path + 'jvs001_VOICEACTRESS100_001.wav')
 print(f'sampling rate:{fs}')
 
@@ -21,5 +22,21 @@ t = time[int(center - (cuttime / 2) * fs) : int(center + (cuttime/2) * fs)]
 plt.plot(t * 1000, x)
 plt.xlabel("time [ms]")
 plt.ylabel("amplitude")
-plt.savefig("waveform.png")
-plt.close()
+# plt.savefig("./data/waveform.png")
+
+# 窓関数をかけてフーリエ変換する
+# ハミング窓をかける
+hamming = np.hamming(len(x))
+x = x * hamming
+
+N = 2048 # FFTのサンプル数(2の累乗で高速化)
+
+# ナイキスト周波数以下のデータを取り出す
+spectre = np.abs(np.fft.fft(x, N))[:N // 2] # フーリエ変換
+fscale = np.fft.fftfreq(N, d = 1.0 / fs)[:N // 2] # フーリエ変換の周波数を取得
+
+plt.plot(fscale, spectre)
+plt.xlabel("frequency [Hz]")
+plt.ylabel("amplitude spectrum")
+# plt.savefig("./data/hamming.png")
+     
